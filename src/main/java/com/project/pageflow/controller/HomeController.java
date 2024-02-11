f11741cdb7059092f1a60121f7fd8b8173008298
BykaWF <yaroslavprozorov213@gmail.com>
2024-02-11 15:47:21 +0100

    Added authentication logic to HomeController

--- src/main/java/com/project/pageflow/controller/HomeController.java
+++ src/main/java/com/project/pageflow/controller/HomeController.java
@@ -4,6 +4,9 @@ import com.project.pageflow.service.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,30 +17,40 @@ public class HomeController {
 
     private AuthenticationManager authenticationManager;
     private StudentService studentService;
+    private static final String LIBRARY_VIEW = "library";
+    private static final String INDEX_VIEW = "index";
+    private static final String ABOUT_VIEW = "about";
+    private static final String LOGIN_VIEW = "login";
+    private static final String SIGN_UP_VIEW = "sign-up";
+    private static final String REGISTRATION_SUCCESS_VIEW = "registration-success";
 
     @GetMapping("/")
-    public String getHomePage(){
-        return "index";
+    public String getHomePage(Authentication authentication) {
+        return getView(authentication, INDEX_VIEW);
     }
 
     @GetMapping("/about")
-    public String getAboutPage(){
-        return "about";
+    public String getAboutPage(Authentication authentication) {
+        return getView(authentication, ABOUT_VIEW);
     }
 
     @GetMapping("/login")
-    public String getLoginPage(){
-        return "login";
+    public String getLoginPage(Authentication authentication) {
+        return getView(authentication, LOGIN_VIEW);
     }
 
-    @GetMapping("/sing-up")
-    public String getSingUpPage(){
-        return "sing-up";
+    @GetMapping("/sign-up")
+    public String getSignUpPage(Authentication authentication) {
+        return getView(authentication, SIGN_UP_VIEW);
     }
 
     @GetMapping("/registration-success")
-    public String getRegistrationSuccess(){
-        return "registration-success";
+    public String getRegistrationSuccess(Authentication authentication) {
+        return getView(authentication, REGISTRATION_SUCCESS_VIEW);
+    }
+
+    private String getView(Authentication authentication, String defaultView) {
+        return authentication.isAuthenticated() ? LIBRARY_VIEW : defaultView;
     }
 
 
